b1c4995100bb82efdf5dd71cc010e541f15f7b76
Sam Meech-Ward <sam@meech-ward.com>
2017-10-10 13:01:03 -0700

    Add a vampire.js file with the starter code for students

---
+++ vampire.js
@@ -0,0 +1,34 @@
+class Vampire {
+  constructor(name) {
+    this.name = name;
+    this.offspring = [];
+    this.creator = null;
+  }
+
+  // Adds the vampire as an offspring of this vampire
+  addOffspring(vampire) {
+
+  }
+
+  // Returns the total number of vampires created by that vampire
+  get numberOfOffspring() {
+
+  }
+
+  // Returns the number of vampires away from the original vampire this vampire is
+  get numberOfVampiresFromOriginal() {
+
+  }
+
+  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+  isMoreSeniorThan(vampire) {
+
+  } 
+
+  // Returns the closest common ancestor of two vampires.
+  closestCommonAncestor(vampire) {
+
+  }
+}
+
+module.exports = Vampire;
