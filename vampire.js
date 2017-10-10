f16dfdfb997511d54003ac94f8b1aa2109eecf34
Sam Meech-Ward <sam@meech-ward.com>
2017-10-10 14:20:22 -0700

    Add the traversal methods

--- vampire.js
+++ vampire.js
@@ -30,6 +30,22 @@ class Vampire {
   closestCommonAncestor(vampire) {
 
   }
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    
+  }
 }
 
 module.exports = Vampire;
+
