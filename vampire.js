00068cb7db184743584a9fdc3ebc3980184251e5
Sam Meech-Ward <sam@meech-ward.com>
2017-12-23 10:37:26 +0000

    Make all the tests pass

--- vampire.js
+++ vampire.js
@@ -10,39 +10,73 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let number = 0;
+    let vampire = this;
+    while (vampire.creator != null) {
+      number++;
+      vampire = vampire.creator;
+    }
+    return number;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
 
   /** Tree traversal methods **/
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
   vampireWithName(name) {
-    
+    if (this.name === name) {
+      return this;
+    }
+
+    for (let child of this.offspring) {
+      let vampire = child.vampireWithName(name);
+      if (vampire) {
+        return vampire;
+      }
+    }
+
+    return null;
   }
 
   // Returns the total number of vampires that exist
   get totalDescendents() {
-    
+    let count = 0;
+    for (let child of this.offspring) {
+      count += child.totalDescendents + 1;
+    }
+    return count;
   }
 
   // Returns an array of all the vampires that were converted after 1980
   get allMillennialVampires() {
+    let vampires = [];
+    if (this.yearConverted > 1980) {
+      vampires.push(this);
+    }
+
+    for (let child of this.offspring) {
+      vampires = vampires.concat(child.allMillennialVampires);
+    }
     
+    return vampires;
   }
 
   /** Stretch **/
@@ -53,7 +87,19 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    let currentAnscestor = this;
+    while (currentAnscestor != null) {
+      let vampireAnscestor = vampire;
+      while (vampireAnscestor != null) {
+        if (vampireAnscestor === currentAnscestor) {
+          return vampireAnscestor;
+        }
+        vampireAnscestor = vampireAnscestor.creator;
+      }
+      currentAnscestor = currentAnscestor.creator;
+    }
+    
+    return null;
   }
 }
 
